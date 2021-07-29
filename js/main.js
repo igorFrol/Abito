e1960c7b93bc7160321591e640d129c19b50b531
igorFrol <88001515+igorFrol@users.noreply.github.com>
2021-07-29 13:30:17 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,17 @@
+
+var swiper = new Swiper(".mySwiper", {
+  loop: true,
+  spaceBetween: 10,
+  slidesPerView: 6,//количество слайдов
+  freeMode: true,
+  watchSlidesVisibility: true,
+  watchSlidesProgress: true,
+});
+var swiper2 = new Swiper(".mySwiper2", {
+  loop: true,
+  spaceBetween: 10,
+  
+  thumbs: {
+    swiper: swiper,
+  },
+});
